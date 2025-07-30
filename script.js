d6748364cf1f6c844b0a29b9e99c3a42de6a5784
alisaig <alisa.ign20@gmail.com>
2025-07-30 13:07:42 +0300

    Create initial functions for operations

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// script
+// operations
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) return "ERROR. DON'T DIVIDE BY 0!"
+    return a / b;
+}
