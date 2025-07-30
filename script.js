b68872c5fdfc446873c73657bd25f93a8daee4ca
alisaig <alisa.ign20@gmail.com>
2025-07-30 13:17:12 +0300

    Add global variables and an operate function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // operations
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
 function add(a, b) {
     return a + b;
 }
@@ -14,4 +18,19 @@ function multiply(a, b) {
 function divide(a, b) {
     if (b === 0) return "ERROR. DON'T DIVIDE BY 0!"
     return a / b;
+}
+
+function operate(num1,num2, operator) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "ERROR";
+    }
 }
