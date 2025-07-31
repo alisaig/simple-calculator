be77902964fee126f800c4e69d1441703e2709d7
alisaig <alisa.ign20@gmail.com>
2025-07-31 17:01:53 +0300

    Rename some buttons and change their classifications

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let num1 = 0;
 let num2 = 0;
 let operator = "";
 
+// const nums = document.querySelectorAll(".num");
+// const operators = document.querySelectorAll(".operator");
+// const clear = document.getElementById("clear");
+// const back = document.getElementById("back");
+const buttons = document.querySelector("#buttons");
+const screen = document.querySelector("#screen");
+
+// Operation functions
 function add(a, b) {
     return a + b;
 }
@@ -20,6 +28,7 @@ function divide(a, b) {
     return a / b;
 }
 
+// General operation function that leads to the correct operation function
 function operate(num1,num2, operator) {
     switch(operator) {
         case "+":
@@ -34,3 +43,32 @@ function operate(num1,num2, operator) {
             return "ERROR";
     }
 }
+
+buttons.addEventListener("click", (event) => {
+    buttonID = event.target.className;
+    buttonClass = event.target.id;
+    
+    switch(button) {
+        case
+    }
+})
+
+
+nums.addEventListener("click", (event) => {
+    key = event.target.id;
+    screen.textContent = key;
+
+
+    function playGame(event) {
+    const humanSelection = event.target.id;
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+
+    if (humanScore === rounds || computerScore === rounds) {
+        buttons.removeEventListener("click", playGame);
+
+        update.textContent = (humanScore === 5) ? "You won!" : "You lost!";
+    }
+}
+})
