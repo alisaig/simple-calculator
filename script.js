1652ac382fc0f2adefc8113d30efb8c0158faba2
alisaig <alisa.ign20@gmail.com>
2025-08-04 20:22:05 +0300

    Handle screen display of operation results
    Where the resulting number exceeds 12 characters
    it is either displayed using exponential notation
    or rounded if the problem is overly long decimals

--- script.js
+++ script.js
@@ -26,7 +26,10 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (b === 0) return "ERROR. DON'T DIVIDE BY 0!"
+    if (b === 0) {
+        clear = true;
+        return "ERROR. DON'T DIVIDE BY 0!";
+    };
     return a / b;
 }
 
@@ -57,6 +60,29 @@ function clearAll() {
     operator = "";
 }
 
+function screenDisplay(dig) {
+    let digStr = dig.toString()
+    const digStrLen = digStr.length;
+
+    if (digStrLen > 12) {
+        const decimalIndx = digStr.indexOf(".");
+        // Handle numbers that are too long because they're too large
+        // No need to check for numbers that are very close to 0 as JS already converts them to exponential notation automatically
+        if (Math.abs(dig) >= 1e12 || (decimalIndx === 11)) {
+            // if ((dig/(10**(digStrLen-1))).toString().length > 9) dig = dig.toExponential(7);
+            dig = dig.toExponential(6);
+            digStr = dig.toString();
+            digStr = digStr.replace(/\.?0+e/, "e");
+
+        // Handle numbers that have overly long decimals
+        } else {
+            dig = dig.toFixed(12-decimalIndx-1);
+            digStr = dig.toString();
+        }
+    }
+    return digStr;
+}
+
 // Event listener for any of the buttons clicked within the #buttons container
 buttons.addEventListener("click", (event) => {
     const buttonClass = event.target.className;
@@ -66,7 +92,7 @@ buttons.addEventListener("click", (event) => {
     // What button was clicked determined by checking its class
     switch(buttonClass) {
         case "num":
-            buttonID = buttonID[1];
+            buttonID = buttonID.slice(1);
             // Value for num1 only directly set by button either:
             // 1. when opening the page
             // 2. after clicking the clear button
@@ -84,27 +110,26 @@ buttons.addEventListener("click", (event) => {
                 num1 = operate(num1, num2, operator);
                 num2 = "";
                 operator = "";
-                screen.textContent = num1;
+                screen.textContent = screenDisplay(num1);
             }
             // Takes care of situation when multiple operator buttons are pressed immediately in succession
             // Only the first operator pressed is registered
             if (operator === "" || lastClickClass === "equals") {
                 operator = buttonID;
+                num2 = "";
                 clear = false;
             };
             break;
         case "equals":
             if (!(num2 === "")) {
                 num1 = operate(num1, num2, operator);
-                // num2 = "";
-                // operator = "";
-                screen.textContent = num1;
+                screen.textContent = screenDisplay(num1);
             // Takes care of situation when equals sign is clicked right after an operator button and before another number button so num2 is empty
             // Operation just happens between two num1s instead
             } else {
                 num2 = num1;
                 num1 = operate(num1, num1, operator);
-                screen.textContent = num1;
+                screen.textContent = screenDisplay(num1);
             }
             break;
         case "clear":
